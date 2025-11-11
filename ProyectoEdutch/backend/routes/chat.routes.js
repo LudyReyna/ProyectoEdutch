0a55e9e09ab665e78e6b38f6f256db64d00c8c80
LudyReyna <lreynato@ucvvirtual.edu.pe>
2025-11-10 21:00:52 -0500

    Delete ProyectoEdutch directory

--- ProyectoEdutch/backend/routes/chat.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { enviarMensaje, obtenerMensajes } from "../controllers/chatController.js";
-
-const router = express.Router();
-
-router.post("/", enviarMensaje);
-router.get("/", obtenerMensajes);
-
-export default router; // 👈 ESTA LÍNEA ES CLAVE
--- ProyectoEdutch/public/js/principal_alumno.js
+++
@@ -1,5 +0,0 @@
-// Simular acción del botón "Buscar Tutor"
-document.getElementById("btnEmparejar").addEventListener("click", () => {
-  alert("Redirigiendo al módulo de emparejamiento con IA...");
-  window.location.href = "emparejamiento.html";
-});
