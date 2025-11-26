a2326129a0405fb3414f8649ec6a333928604ca9
MireAlexa <alexandramirellacardenas@gmail.com>
2025-11-26 00:57:33 -0500

    Actualización de codigo

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,14 +1,28 @@
 export default {
-    testEnvironment: "node",
-    transform: {},
-    modulePathIgnorePatterns: [
-        "<rootDir>/public/",
-        "<rootDir>/coverage/",
-        "<rootDir>/backend/coverage/",
-        "<rootDir>/coverage-report/",
-        "<rootDir>/backend/coverage-report/"
-    ],
+  testEnvironment: "node",
+
+  // No usar Babel
+  transform: {},
+
+  // Ignorar node_modules COMPLETAMENTE
+  transformIgnorePatterns: [
+    "/node_modules/"
+  ],
+
+  // Ignorar cualquier carpeta "config" que pueda confundir a Jest
+  modulePathIgnorePatterns: [
+    "<rootDir>/config",
+    "<rootDir>/node_modules"
+  ],
+
+  // Ruta para los tests
+  testMatch: ["**/tests/**/*.test.js"],
+
+  verbose: false
 };
+
+
+
 
 
 
